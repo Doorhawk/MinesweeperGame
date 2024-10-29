6b588503f697c1062f1efeb5c9a0a3f2b203e397
Doorhawk <lekonse.a21@gmail.com>
2024-10-29 15:39:51 +0300

    Update random.h

--- bomber/source/random.h
+++ bomber/source/random.h
@@ -3,11 +3,11 @@
 
 class Random {
 public:
-    Random();  // Инициализируем генератор случайным значением от random_device
+    Random();  
 
     int get_random_int(int min, int max);
     float get_random_float(float min, float max);
 private:
-    std::random_device rd;  // Обеспечивает случайное зерно для генератора
-    std::mt19937 gen;       // Создаем генератор Mersenne Twister
+    std::random_device rd;  
+    std::mt19937 gen;     
 };
