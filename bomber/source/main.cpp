38e148d39e5b10e672dbd7c0b4bdc2b1ab12514a
Doorhawk <lekonse.a21@gmail.com>
2024-10-29 15:38:46 +0300

    Update main.cpp

--- bomber/source/main.cpp
+++ bomber/source/main.cpp
@@ -2,18 +2,9 @@
 #include "WindowManager.h"
 #include "game.h"
 
-
-
-// разобраться с ифами в отрисовке тайлов и вообще может её на несколько функций разбить
-// кастом мап?
-
-
-
 int main()
 {
     WindowManager win;
     win.updateWindow();
-
-
     return 0;
 }
